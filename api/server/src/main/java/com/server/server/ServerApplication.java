fce7c2b6338a6b988e0fdbee8e14bb5f7d468c90
Nikola Mitic <nmitic304@gmail.com>
2025-03-27 18:51:30 -0400

    nnikola

--- api/server/src/main/java/com/server/server/ServerApplication.java
+++ api/server/src/main/java/com/server/server/ServerApplication.java
@@ -1,8 +1,13 @@
 package com.server.server;
 
+import java.beans.JavaBean;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@JavaBean
 @SpringBootApplication
 public class ServerApplication {
 
