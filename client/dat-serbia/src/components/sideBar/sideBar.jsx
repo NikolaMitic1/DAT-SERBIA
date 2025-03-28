c442c2cc864190831b9d578067ef93b111819e34
Nikola Mitic <nmitic304@gmail.com>
2025-03-28 07:59:05 -0400

    3/28

---
+++ client/dat-serbia/src/components/sideBar/sideBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function sideBar() {
+  return (
+    <div className="navbar">
+        <div className="logo">
+
+        </div>
+
+        <div className="menu">
+
+        </div>
+
+        <div className="foot">
+            
+        </div>
+    </div>
+  )
+}
